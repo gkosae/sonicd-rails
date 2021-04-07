e2fbdd53e20f3313326aeb65d97346f0d248cbda
George Osae <gkosae@gmail.com>
2021-04-07 10:24:25 +0000

    Reduce log level to warn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,6 +98,7 @@ Rails.application.configure do
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.log_level = :warn
   end
 
   # Do not dump schema after migrations.
