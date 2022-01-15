95eefa15eae134008cf675aa65b1f22b90f7b5c4
George Osae <gkosae@gmail.com>
2022-01-15 18:48:39 +0000

    Updated sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,12 @@
+redis_conf = { url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" } }
+
 Sidekiq.configure_server do |config|
   config.options[:concurrency] = (ENV.fetch('SIDEKIQ_CONCURRENCY') { '4' }).to_i
+  config.redis = redis_conf
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = redis_conf
 end
 
 schedule_file = 'config/schedule.yml'
